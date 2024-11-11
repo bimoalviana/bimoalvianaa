03aa8a45f550879aee910ee845114fa75b5e1df6
bimoalviana <bimoalviana05@gmail.com>
2024-11-11 15:58:30 +0700

    Create Soal_3.py

---
+++ Soal_3.py
@@ -0,0 +1,9 @@
+# NAMA : BIMO ALVIANA SOPIAN 
+# NIM  : 2403010071
+# KELAS:C
+kata_pertama = input("Masukkan kata pertama: ")
+kata_kedua = input("Masukkan kata kedua: ")
+
+gabungan = kata_pertama + kata_kedua
+
+print(f"Hasil penggabungan: {gabungan}")

80823c895e03bd9d76724223b2667f1aa45a0b3b
bimoalviana <bimoalviana05@gmail.com>
2024-11-11 16:22:21 +0700

    Soal_4.py

--- Soal_4.py
+++ Soal_4.py
@@ -1,7 +1,7 @@
 # NAMA : BIMO ALVIANA SOPIAN
 # NIM  : 2403010071
 # KELAS: C
-kalimat = input("Masukkan kalimat: ")
+kalimat = input("Selamat datang di dunia pemrograman: ")
 
 jumlah_vokal = 0
 
@@ -11,4 +11,4 @@ for huruf in kalimat:
     if huruf in vokal:
         jumlah_vokal += 1
 
-print(f"Jumlah huruf vokal: {jumlah_vokal}")
+print(f"13: {jumlah_vokal}")

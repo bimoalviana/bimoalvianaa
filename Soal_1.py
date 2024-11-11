70ef6666a9d957c36ee6e7425b3e56749a59f93d
bimoalviana <bimoalviana05@gmail.com>
2024-11-11 15:53:11 +0700

    Soal_1.py

---
+++ Soal_1.py
@@ -0,0 +1,11 @@
+# NAMA: BIMO ALVIANA SOPIAN
+# NIM : 2403010071
+# KELAS:C
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print(f"Luas persegi panjang: {luas}")
+print(f"Keliling persegi panjang: {keliling}")

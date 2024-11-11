55145b3f5ad16084a7cca11bbdff374a4224e5a4
bimoalviana <bimoalviana05@gmail.com>
2024-11-11 15:50:06 +0700

    Soal_2.py

---
+++ Soal_2.py
@@ -0,0 +1,8 @@
+# NAMA:BIMO ALVIANA SOPIAN
+# NIM :2403010071
+# KELAS:c
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+
+fahrenheit = (celcius * (9/5)) + 32
+
+print(f"Suhu dalam Fahrenheit: {fahrenheit}")

94672c9c1ea8cbe498983648fcce1b71cbdb5627
bimoalviana <bimoalviana05@gmail.com>
2024-11-11 16:16:16 +0700

    Soal_1.py

--- Soal_1.py
+++ Soal_1.py
@@ -1,11 +1,11 @@
 # NAMA: BIMO ALVIANA SOPIAN
 # NIM : 2403010071
 # KELAS:C
-panjang = float(input("Masukkan panjang: "))
-lebar = float(input("Masukkan lebar: "))
+panjang = float(input("5: "))
+lebar = float(input("3: "))
 
 luas = panjang * lebar
 keliling = 2 * (panjang + lebar)
 
-print(f"Luas persegi panjang: {luas}")
-print(f"Keliling persegi panjang: {keliling}")
+print(f"15.0: {luas}")
+print(f"16.0: {keliling}")
